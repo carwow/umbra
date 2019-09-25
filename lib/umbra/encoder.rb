4f2bf05fb16f20d0331cf1a7a161f376e955b037
Christian Gregg <c_arlt@hotmail.com>
2019-09-25 20:12:29 +0100

    Split out Umbra::Encoder#call
    Means subclasses can just override #to_h and not care about the json
    encoding
    
    also do some memoizing

--- lib/umbra/encoder.rb
+++ lib/umbra/encoder.rb
@@ -11,32 +11,37 @@ module Umbra
       @status, @headers, @body = response
     end
 
-    def call
-      MultiJson.dump(
-        'request' => {
-          'scheme' => @env.fetch('rack.url_scheme'),
-          'host' => @env['HTTP_HOST'] || @env.fetch('SERVER_NAME'),
-          'uri' => @env.fetch('REQUEST_URI'),
-          'port' => @env.fetch('SERVER_PORT'),
-          'method' => @env.fetch('REQUEST_METHOD'),
-          'query' => @env.fetch('QUERY_STRING'),
-          'script_name' => @env.fetch('SCRIPT_NAME'),
-          'path_info' => @env.fetch('PATH_INFO'),
-          'headers' => request_headers,
-          'body' => request_body
-        },
-        'response' => {
-          'status' => @status,
-          'headers' => @headers,
-          'body' => body_string
+    def to_h
+      @to_h ||=
+        {
+          'request' => {
+            'scheme' => @env.fetch('rack.url_scheme'),
+            'host' => @env['HTTP_HOST'] || @env.fetch('SERVER_NAME'),
+            'uri' => @env.fetch('REQUEST_URI'),
+            'port' => @env.fetch('SERVER_PORT'),
+            'method' => @env.fetch('REQUEST_METHOD'),
+            'query' => @env.fetch('QUERY_STRING'),
+            'script_name' => @env.fetch('SCRIPT_NAME'),
+            'path_info' => @env.fetch('PATH_INFO'),
+            'headers' => request_headers,
+            'body' => request_body
+          },
+          'response' => {
+            'status' => @status,
+            'headers' => @headers,
+            'body' => body_string
+          }
         }
-      )
+    end
+
+    def call
+      @call ||= MultiJson.dump(to_h)
     end
 
     private
 
     def request_headers
-      @env.select { |k, _| k.start_with?('HTTP_') }
+      @request_headers ||= @env.select { |k, _| k.start_with?('HTTP_') }
     end
 
     def request_body
@@ -44,11 +49,14 @@ module Umbra
     end
 
     def body_string
-      str = []
+      @body_string ||=
+        begin
+          str = []
 
-      @body.each { |x| str << x.to_s }
+          @body.each { |x| str << x.to_s }
 
-      str.join('')
+          str.join('')
+        end
     end
   end
 end
